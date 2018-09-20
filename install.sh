d17779fbc43a01be00b024b1b8a68d94cbcaa66d
Nathan McCorkle <njmccorkle@gmail.com>
2018-09-20 15:43:15 +0000

    add install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##Assign existing hostname to $hostn
+#hostn=$(cat /etc/hostname)
+
+##change hostname in /etc/hosts & /etc/hostname
+#sudo sed -i "s/$hostn/BBQ/g" /etc/hosts
+#sudo sed -i "s/$hostn/BBQ/g" /etc/hostname
+
+#apt-get update
+#apt-get -y upgrade
+#apt-get install -y python-pip
+
+##install nginx
+#apt-get install -y nginx
