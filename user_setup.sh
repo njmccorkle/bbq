0ab50311eececcd0f11c02a53b809e4cc127720c
njmccorkle <35677754+njmccorkle@users.noreply.github.com>
2018-09-21 22:34:35 -0500

    Update user_setup.sh

--- user_setup.sh
+++ user_setup.sh
@@ -1,3 +1,4 @@
 mkdir .ssh
 scp -o StrictHostKeyChecking=no root@freenas:///mnt/Storage/conf/vimrc ~/.vimrc
 scp -o StrictHostKeyChecking=no root@freenas:///mnt/Storage/conf/authorized_keys2 ~/.ssh/authorized_keys2
+wget https://raw.githubusercontent.com/njmccorkle/bbq/master/install.sh
