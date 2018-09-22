b5234eea32729210bd68f8539ab768c33ed55a14
njmccorkle <35677754+njmccorkle@users.noreply.github.com>
2018-09-21 22:42:16 -0500

    Update user_setup.sh

--- user_setup.sh
+++ user_setup.sh
@@ -5,7 +5,12 @@ systemctl start ssh
 mkdir .ssh
 scp -o StrictHostKeyChecking=no root@freenas:///mnt/Storage/conf/vimrc ~/.vimrc
 scp -o StrictHostKeyChecking=no root@freenas:///mnt/Storage/conf/authorized_keys2 ~/.ssh/authorized_keys2
-wget https://raw.githubusercontent.com/njmccorkle/bbq/master/install.sh
 
+#set timezone
 echo "America/Chicago" > /etc/timezone    
 dpkg-reconfigure -f noninteractive tzdata
+
+
+
+
+wget https://raw.githubusercontent.com/njmccorkle/bbq/master/install.sh
