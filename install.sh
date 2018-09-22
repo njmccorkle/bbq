47d8043e77d4e361bbeb7d71059ebf771a9f2f1c
njmccorkle <35677754+njmccorkle@users.noreply.github.com>
2018-09-21 22:23:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,36 +4,37 @@
 hostname_current=$(cat /etc/hostname)
 hostname_new="bbq"
 
+##enable and start SSH
+systemctl enable ssh
+systemctl start ssh
+
 ##change hostname in /etc/hosts & /etc/hostname
 sudo sed -i "s/$hostname_current/$hostname_new/g" /etc/hosts
 sudo sed -i "s/$hostname_current/$hostname_new/g" /etc/hostname
 
-##add influxdb key and repos
-curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-
 ##update from repos, upgrade, and install
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install build-essential python-dev python-pip python-smbus git nginx ufw influxdb chronograf telegraf
+apt-get -y install build-essential python3-dev python3-pip python3-smbus git nginx ufw python3-rpi.gpio
 
+##add influxdb, chronograf, telegraf
+#curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+#echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+#apt-get-y install influxdb chronograf telegraf
 ##Enable and start new services
-systemctl daemon-reload
-systemctl enable influxdb
-systemctl start influxdb
-systemctl enable chronograf
-systemctl start chronograf
-systemctl enable telegraf
-systemctl start telegraf
+#systemctl daemon-reload
+#systemctl enable influxdb
+#systemctl start influxdb
+#systemctl enable chronograf
+#systemctl start chronograf
+#systemctl enable telegraf
+#systemctl start telegraf
 
 ##setup ufw
 ufw allow ssh
 ufw allow http
-ufw allow 8888
+#ufw allow 8888  #chronograf
 ufw --force enable
-
-##Python libraries for pi
-pip install RPi.GPIO
 
 ##create a folder to store adafruit library files. download and install
 mkdir adafruit_python_max31855
@@ -42,6 +43,4 @@ cd adafruit_python_max31855
 python setup.py install
 cd ..
 
-##Install Python libraries
-pip install RPi.GPIO
-pip install influxdb
+reboot
