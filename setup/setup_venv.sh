f234a6ba8fd6d4f7e8261e8f69a7d5f0aa85874f
Nathan McCorkle <njmccorkle@gmail.com>
2018-09-22 23:19:59 -0500

    setup script updates

--- setup/setup_venv.sh
+++ setup/setup_venv.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-virtualenv . 
+#virtualenv . 
 ##source ~/q-meter/bin/activate
-~/q-meter/bin/pip install RPi.GPIO
+#~/q-meter/bin/pip install RPi.GPIO
 
 
 ##create a folder to store adafruit library files. download and install
-mkdir adafruit_python_max31855
-git clone https://github.com/adafruit/Adafruit_Python_MAX31855.git adafruit_python_max31855
-cd adafruit_python_max31855
-~/q-meter/bin/python setup.py install
-cd ..
+#mkdir adafruit_python_max31855
+#git clone https://github.com/adafruit/Adafruit_Python_MAX31855.git adafruit_python_max31855
+#cd adafruit_python_max31855
+#~/q-meter/bin/python setup.py install
+#cd ..
 
+~/q-meter/bin/pip install uwsgi flask
