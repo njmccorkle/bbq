e3fd141c61903fd017aae8ad53e4890bbbac64a0
Nathan McCorkle <njmccorkle@gmail.com>
2018-09-24 21:36:39 -0500

    fix errors in setup

--- setup/setup_system.sh
+++ setup/setup_system.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
 ##Assign existing hostname to $hostn
-#hostname_current=$(cat /etc/hostname)
-#hostname_new="qmeter"
+hostname_current=$(cat /etc/hostname)
+hostname_new="test"
 
 ##change hostname in /etc/hosts & /etc/hostname
-#sudo sed -i "s/$hostname_current/$hostname_new/g" /etc/hosts
-#sudo sed -i "s/$hostname_current/$hostname_new/g" /etc/hostname
+sudo sed -i "s/$hostname_current/$hostname_new/g" /etc/hosts
+sudo sed -i "s/$hostname_current/$hostname_new/g" /etc/hostname
 
 ##update from repos, upgrade, and install
-#apt-get -y update
-#apt-get -y upgrade
-#apt-get -y install build-essential vim python3-dev python3-pip python3-smbus git nginx ufw
-#apt-get -y autoremove
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install build-essential
+apt-get -y install vim 
+apt-get -y install python3-dev
+apt-get -y install python3-pip
+apt-get -y install python3-smbus
+apt-get -y install git
+apt-get -y install nginx
+apt-get -y install ufw
+ 
+apt-get -y autoremove
 
 ##setup ufw
-#ufw allow ssh
-#ufw allow http
-#ufw --force enable
+ufw allow ssh
+ufw allow http
+ufw --force enable
 
-#cp /home/pi/qmeter/files/qmeter.service /etc/systemd/system/qmeter.service
-#systemctl daemon-reload
-#systemctl enable qmeter.service
-#systemctl start qmeter.service
+sudo pip3 install virtualenv
+
+cp /home/pi/qmeter/files/qmeter.service /etc/systemd/system/qmeter.service
+systemctl daemon-reload
+systemctl enable qmeter.service
+systemctl start qmeter.service
 
 cp /home/pi/qmeter/files/qmeter.nginx /etc/nginx/sites-available/qmeter
 ln -s /etc/nginx/sites-available/qmeter /etc/nginx/sites-enabled
